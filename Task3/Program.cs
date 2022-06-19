0e93fe216b9dd1e6a1f99de67063577d13a467b0
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-19 11:10:24 +0300

    3е задание выполнено для "int" переменных

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+int number;
+
+Console.WriteLine("Введите число");
+number =Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
