a3fc0ccdd2b98e3c718c375e27f77d39645ee640
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-19 11:32:01 +0300

    Пляска со списками

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+int number;
+
+Console.WriteLine("Введите число");
+number =Convert.ToInt32(Console.ReadLine());
+while (number > 0)
+{
+    if (number % 2 == 0)
+    {
+     Console.Write.(number + ", ");
+     number --;
+    }
+    else
+    {
+        number --;
+}
+}
