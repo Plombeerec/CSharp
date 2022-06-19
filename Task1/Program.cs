178be2e23d6075c27d6adb88872cbd307a8a9f4f
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-19 10:28:34 +0300

    First task done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на 
+//вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a , b;
+Console.WriteLine("Введите сравниваемые числа");
+a =Convert.ToInt32(Console.ReadLine());
+b =Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Большее значение = " + a);
+}
+else
+{
+    Console.WriteLine("Большее значение = " + b);
+}
