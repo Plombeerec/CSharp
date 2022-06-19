661e3d6dc6fab9a54fd552df165b68c115f77492
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-19 10:55:49 +0300

    Задание № 2 выполнено

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+double a , b, c;
+
+Console.WriteLine("Введите три числа");
+a =Convert.ToInt32(Console.ReadLine());
+b =Convert.ToInt32(Console.ReadLine());
+c =Convert.ToInt32(Console.ReadLine());
+
+if ((a > b) & (a > c))
+{
+    Console.WriteLine("Наибольшее число = " + a);
+}
+if ((b > a) & (b > c))
+{
+    Console.WriteLine("Наибольшее число = " + b);
+}
+if ((c > b) & (c > a))
+{
+    Console.WriteLine("Наибольшее число = " + c);
+}
